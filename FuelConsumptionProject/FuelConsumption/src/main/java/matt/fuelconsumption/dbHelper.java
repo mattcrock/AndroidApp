9022478c48b21b8c1f9569889e7c8c08cba31264
mattcrock <crock.matt@gmail,com>
2013-07-05 08:53:47 +0200

    Adding and dropping of consumtption table

--- FuelConsumptionProject/FuelConsumption/src/main/java/matt/fuelconsumption/dbHelper.java
+++ FuelConsumptionProject/FuelConsumption/src/main/java/matt/fuelconsumption/dbHelper.java
@@ -1,7 +1,36 @@
 package matt.fuelconsumption;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Matt on 7/5/13.
  */
-public class dbHelper {
+public class dbHelper extends SQLiteOpenHelper {
+
+    static final String dbName="FuelConsumption";
+    static final String consumptionTable="Consumption";
+    static final String colID="ID";
+    static final String colKm="Kilometers";
+    static final String colL="Litres";
+    static final String colKmL="KilometersPerLitre";
+    static final String colLkm="LitresPer100Kilometers";
+
+    public dbHelper(Context context){
+        super(context,dbName,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //create all the tables that will be needed
+        db.execSQL("CREATE TABLE " + consumptionTable + " (" + colID + " INTEGER PRIMARY KEY , " + colKm + "INTEGER NOT NULL , " + colL + " INTEGER NOT NULL , " +
+                colKmL + " INTEGER , " + colLkm + " INTEGER , " + ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //reverse of the table create
+        db.execSQL("DROP TABLE IF EXISTS " + consumptionTable);
+    }
 }
